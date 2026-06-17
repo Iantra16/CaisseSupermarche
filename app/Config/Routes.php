d0d53b241278857fb968547bc563806fcd5e7168
Iantra <iantrarak@gmail.com>
2026-06-17 09:55:13 +0300

    vue caisse

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-
-$routes->get('/etudiant', 'Etudiant::index');
-
-$routes->get('/etudiant/(:num)', 'Etudiant::show/$1');
-
-$routes->resource('api/etudiant');
+$routes->get('/', 'CaisseController::index');
